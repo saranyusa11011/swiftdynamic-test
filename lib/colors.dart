aed54531c49da0e62a167bed386834bf77afd554
saranyu11011 <saranyusa@kkumail.com>
2024-02-28 19:32:53 +0700

    a

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+
+
+
+
 class AppColors {
   static final Color textColor = const Color(0xFFccc7c5);
   static final Color mainColor = const Color(0xFF5b5f97);
